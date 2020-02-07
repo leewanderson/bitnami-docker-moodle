206f41977db2be2ed7039aa60037173cd27f7def
leewanderson <leewanderson@yahoo.co.uk>
2020-02-07 10:57:51 +0000

    removed unnecessary shellscripts

--- mariadb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker stop bitnami-docker-moodle_mariadb_1
-docker run -p 3306:3306 --name <bitnami-docker-moodle_mariadb_1> bitnami/mariadb:10.1
--- moodle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker stop bitnami-docker-moodle_moodle_1
-docker pull bitnami/moodle
-docker run -p 80:80 --name < bitnami-docker-moodle_moodle_1 > moodle:latest
